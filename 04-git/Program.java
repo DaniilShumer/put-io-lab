e187efdf639eb382ba4c17827c98a84a8e553557
DaniilShumer <daniil.shumer@gmail.com>
2023-10-30 17:36:09 +0100

    Pierwsza zmiana zad6

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,7 +1,8 @@
+// plik program, zadanie 6
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World i Politechnika");
         int x = 15;
-        System.out.println("Hello integer " + x);
+        System.out.println("Hello " + x);
     }
 }
