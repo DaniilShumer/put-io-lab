cf43841f894ba532c07a972dd0c7aa54ad506db4
DaniilShumer <daniil.shumer@gmail.com>
2023-10-30 16:51:35 +0100

    Add output of a variable to Program.java

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args){
         System.out.print("Hello World and Hello Politechnika ");
         int x = 10;
+        System.out.println(x);
     }
 }
